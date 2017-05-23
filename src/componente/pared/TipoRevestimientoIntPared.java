d6752c7ef61314939f6524dade3d28738df4a0bd
ramoncenturion <ramoncenturion74@gmail.com>
2017-05-22 23:56:49 -0300

    arreglo utf8

--- src/componente/pared/TipoRevestimientoIntPared.java
+++ src/componente/pared/TipoRevestimientoIntPared.java
@@ -2,10 +2,10 @@ package componente.pared;
 
 public enum TipoRevestimientoIntPared {
 	LADRILLO_VISTA("ladrillo a la vista",1), PIEDRA_PROYECTADA("piedra proyectada",2),
-	ESTUCO_VENECIANO("estuco veneciano",3), VINÕLICOS("vinÌlicos",4),MICROCEMENTO("microcemento",1), 
+	ESTUCO_VENECIANO("estuco veneciano",3), VINILICOS("vinilicos",4),MICROCEMENTO("microcemento",1), 
 	VENECITAS("venecitas",2), ESMALTADO("esmaltado",3), REVOQUE("revoque",4),MADERA("madera",1), 
-	CER¡MICA("cer·mica",2),HORMIG”N_PULIDO("hormigÛn pulido",3), PIEDRA_NATURAL("piedra natural",4),
-	PAPEL_PINTADO("papel pintado",1), AZULEJOS("azulejos",2), M¡RMOL("m·rmol",3);
+	CERAMICA("ceramica",2),HORMIGON_PULIDO("hormigon pulido",3), PIEDRA_NATURAL("piedra natural",4),
+	PAPEL_PINTADO("papel pintado",1), AZULEJOS("azulejos",2), M√ÅRMOL("m√°rmol",3);
 	
 	private String descripcion;
 	private	int valor;
--- src/componente/piso/TipoPiso.java
+++ src/componente/piso/TipoPiso.java
@@ -3,7 +3,7 @@ package componente.piso;
 public enum TipoPiso {
 	
 	LADRILLO("Ladrillo",1),MADERA("Madera",2), CERAMICOS("Ceramicos",3), 
-	MARMOL("Marmol",4), CEMENTO("cemento",5),CEMENTO_Y_HORMIGÓN("Cemento y Hormigon",6), 
+	MARMOL("Marmol",4), CEMENTO("cemento",5),CEMENTO_Y_HORMIGON("Cemento y Hormigon",6), 
 	MOSAICOS("Mosaicos",7), ENTABLONADA("Entablonada",8), FLOTANTE("Flotante",9);
 	
 	private String descripcion;
